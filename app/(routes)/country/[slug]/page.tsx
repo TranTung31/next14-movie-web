d6208272c40ffb069eaefb65a04d2b79f36e9190
Your Name <you@example.com>
2024-09-07 22:04:34 +0700

    feat: code responsive phần header

---
+++ app/(routes)/country/[slug]/page.tsx
@@ -0,0 +1,24 @@
+import { getCountryMovie } from '@/app/(routes)/actions'
+import GenreMovie from '@/components/genre-movie'
+
+const CountryPage = async ({
+  params,
+  searchParams,
+}: {
+  params: { [key: string]: string }
+  searchParams: { [key: string]: string }
+}) => {
+  const countryMovie = await getCountryMovie(params?.slug, searchParams?.page)
+
+  return (
+    <div className="w-full min-h-screen bg-[#181818] px-1 lg:px-20 py-10">
+      <GenreMovie
+        data={countryMovie}
+        currentPage={Number(searchParams?.page) || 1}
+        breadCrumbTitle="Country Movie"
+      />
+    </div>
+  )
+}
+
+export default CountryPage
---
+++ lib/constants.ts
@@ -0,0 +1,187 @@
+export const movieGenres: {
+  title: string
+  href: string
+  description: string
+}[] = [
+  {
+    title: 'Hành động',
+    href: '/genre/hanh-dong?page=1',
+    description: '',
+  },
+  {
+    title: 'Phiêu lưu',
+    href: '/genre/phieu-luu?page=1',
+    description: '',
+  },
+  {
+    title: 'Hoạt hình',
+    href: '/genre/hoat-hinh?page=1',
+    description: '',
+  },
+  {
+    title: 'Hài',
+    href: '/genre/phim-hai?page=1',
+    description: '',
+  },
+  {
+    title: 'Hình sự',
+    href: '/genre/hinh-su?page=1',
+    description: '',
+  },
+  {
+    title: 'Tài liệu',
+    href: '/genre/tai-lieu?page=1',
+    description: '',
+  },
+  {
+    title: 'Chính kịch',
+    href: '/genre/chinh-kick?page=1',
+    description: '',
+  },
+  {
+    title: 'Gia đình',
+    href: '/genre/gia-dinh?page=1',
+    description: '',
+  },
+  {
+    title: 'Giả tưởng',
+    href: '/genre/gia-tuong?page=1',
+    description: '',
+  },
+  {
+    title: 'Lịch sử',
+    href: '/genre/lich-su?page=1',
+    description: '',
+  },
+  {
+    title: 'Kinh dị',
+    href: '/genre/kinh-di?page=1',
+    description: '',
+  },
+  {
+    title: 'Nhạc',
+    href: '/genre/phim-nhac?page=1',
+    description: '',
+  },
+  {
+    title: 'Bí ẩn',
+    href: '/genre/bi-an?page=1',
+    description: '',
+  },
+  {
+    title: 'Lãng mạn',
+    href: '/genre/lang-man?page=1',
+    description: '',
+  },
+  {
+    title: 'Viễn tưởng',
+    href: '/genre/khoa-hoc-vien-tuong?page=1',
+    description: '',
+  },
+  {
+    title: 'Gay cấn',
+    href: '/genre/gay-can?page=1',
+    description: '',
+  },
+  {
+    title: 'Chiến tranh',
+    href: '/genre/chien-tranh?page=1',
+    description: '',
+  },
+  {
+    title: 'Tâm lý',
+    href: '/genre/tam-ly?page=1',
+    description: '',
+  },
+  {
+    title: 'Tình cảm',
+    href: '/genre/tinh-cam?page=1',
+    description: '',
+  },
+  {
+    title: 'Cổ trang',
+    href: '/genre/co-trang?page=1',
+    description: '',
+  },
+  {
+    title: 'Miền tây',
+    href: '/genre/mien-tay?page=1',
+    description: '',
+  },
+  {
+    title: 'Phim 18+',
+    href: '/genre/phim-18?page=1',
+    description: '',
+  },
+]
+
+export const movieCountries: {
+  title: string
+  href: string
+  description?: string
+}[] = [
+  {
+    title: 'Âu Mỹ',
+    href: '/country/au-my?page=1',
+  },
+  {
+    title: 'Anh',
+    href: '/country/anh?page=1',
+  },
+  {
+    title: 'Trung Quốc',
+    href: '/country/trung-quoc?page=1',
+  },
+  {
+    title: 'Indonesia',
+    href: '/country/indonesia?page=1',
+  },
+  {
+    title: 'Việt Nam',
+    href: '/country/viet-nam?page=1',
+  },
+  {
+    title: 'Pháp',
+    href: '/country/phap?page=1',
+  },
+  {
+    title: 'Hong Kong',
+    href: '/country/hong-kong?page=1',
+  },
+  {
+    title: 'Hàn Quốc',
+    href: '/country/han-quoc?page=1',
+  },
+  {
+    title: 'Nhật Bản',
+    href: '/country/nhat-ban?page=1',
+  },
+  {
+    title: 'Thái Lan',
+    href: '/country/thai-lan?page=1',
+  },
+  {
+    title: 'Đài Loan',
+    href: '/country/dai-loan?page=1',
+  },
+  {
+    title: 'Nga',
+    href: '/country/nga?page=1',
+  },
+  {
+    title: 'Hà Lan',
+    href: '/country/ha-lan?page=1',
+  },
+  {
+    title: 'Philippines',
+    href: '/country/philippines?page=1',
+  },
+  {
+    title: 'Ấn Độ',
+    href: '/country/an-do?page=1',
+  },
+  {
+    title: 'Quốc gia khác',
+    href: '/country/quoc-gia-khac?page=1',
+  },
+]
